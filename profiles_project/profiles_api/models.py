2acd770aba85d4f54b014fad07e758672eaa25b8
sanskar1991 <sanskarj123@gmail.com>
2020-12-06 01:36:15 +0530

    Added custom usermodel and manager

--- profiles_project/profiles_api/models.py
+++ profiles_project/profiles_api/models.py
@@ -1,3 +1,59 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
+
+
+class UserProfileManager(BaseUserManager):
+    """Custom manager to manage UserProfile class"""
+    
+    def create_user(self, email, name, password=None):
+        """create a user by its email"""
+        if not email:
+            raise ValueError('User must have an email address')
+        
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name)
+        
+        user.set_password(password)
+        user.save(using=self._db)
+        
+        return user
+        
+    def create_superuser(self, email, name, password):
+        """create a superuser"""
+        user = self.create_user(email, name, password)
+        
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+        
+        return user
+    
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    """Database model for user in the system"""
+    email = models.EmailField(max_length=255, unique=True)
+    # first_name = models.CharField(max_length=15)
+    # last_name = models.CharField(max_length=15)
+    name = models.CharField(max_length=25)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    # full_name = first_name + ' ' + last_name
+    
+    objects = UserProfileManager()
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+    def  get_full_name(self):
+        """Retrives full name of a user"""
+        return self.name
+    
+    def get_short_name(self):
+        """Retrives short name of a user"""
+        return self.name
+    
+    def __str__(self):
+        """Return string representation of user"""
+        return self.email
--- profiles_project/profiles_project/settings.py
+++ profiles_project/profiles_project/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # 'rest_framework',
-    # 'rest_framework.authtoken'
+    'rest_framework',
+    'rest_framework.authtoken'
     
 ]
 
@@ -122,3 +122,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = 'profiles_api.UserProfile'

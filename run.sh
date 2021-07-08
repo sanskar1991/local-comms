7e24fb4a6f862df8e1341ad635904dbab23a2162
sanskar1991 <sanskarj123@gmail.com>
2021-07-08 12:19:21 +0530

    Added docker installation steps in bash script

--- run.sh
+++ run.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-echo "Build starting.."
+
+yum update -y
+# amazon-linux-extras install docker
+yum install docker
+service docker start
+sudo curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-`uname -s`-`uname -m` | sudo tee /usr/local/bin/docker-compose > /dev/null
+sudo chmod +x /usr/local/bin/docker-compose
+ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 docker-compose up
